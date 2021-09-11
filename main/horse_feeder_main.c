6653c683125f63a59469715321a531218693b370
Santeri Porrasmaa <porrasmaa@gmail.com>
2021-09-11 10:45:23 +0300

    get_time works

--- main/horse_feeder_main.c
+++ main/horse_feeder_main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "wifi_connect.h"
+#include "ntp_client.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
@@ -7,8 +10,11 @@
 void app_main(void)
 {
     int wifi_conn_stat=0;
+    char* env;
     printf("Initializing WiFi\n");
     wifi_conn_stat=conn_wifi();
+    env=getenv("TZ");
+    printf("Env TZ: %s\n", env);
     if (wifi_conn_stat != 0) {
         printf("Connection failed!\n");
         //printf("Trying again!\n");
@@ -16,5 +22,11 @@ void app_main(void)
     }   
     else {
         printf("Connected to WiFi\n");
+        printf("Getting current time\n");
+        while (1) {
+          time_t time = get_time();
+          printf("Current time is %s", ctime(&time));
+          vTaskDelay(5000/portTICK_PERIOD_MS);
+        }
     }
 }
---
+++ main/ntp_client.c
@@ -0,0 +1,146 @@
+/*
+ *
+ * (C) 2014 David Lettier.
+ *
+ * http://www.lettier.com/
+ *
+ * NTP client.
+ *
+ * Compiled with gcc version 4.7.2 20121109 (Red Hat 4.7.2-8) (GCC).
+ *
+ * Tested on Linux 3.8.11-200.fc18.x86_64 #1 SMP Wed May 1 19:44:27 UTC 2013 x86_64 x86_64 x86_64 GNU/Linux.
+ *
+ * To compile: $ gcc main.c -o ntpClient.out
+ *
+ * Usage: $ ./ntpClient.out
+ *
+ * Original author: David Letter
+ * Modified by: Santeri Porrasmaa
+ * 
+ * License from original repo (https://github.com/lettier/ntpclient)
+
+ *  BSD 3-Clause License
+ *  
+ *  Copyright (c) 2014, David Lettier
+ *  All rights reserved.
+ *  
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *  
+ *  * Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *  
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *  
+ *  * Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *  
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#include "ntp_client.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+
+
+time_t get_time()
+{
+    int sockfd, n; // Socket file descriptor and the n return result from writing/reading from the socket.
+
+    int portno = 123; // NTP UDP port number.
+
+    // NTP server hostnames. Use MIKES server in Finland.
+    const char* host_name = "us.pool.ntp.org";//"time.mikes.fi";
+
+    // Create and zero out the packet. All 48 bytes worth.
+
+    ntp_packet packet = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+    memset( &packet, 0, sizeof( ntp_packet ) );
+
+    // Set the first byte's bits to 00,011,011 for li = 0, vn = 3, and mode = 3. The rest will be left set to zero.
+
+    *( ( char * ) &packet + 0 ) = 0x1b; // Represents 27 in base 10 or 00011011 in base 2.
+
+    // Create a UDP socket, convert the host-name to an IP address, set the port number,
+    // connect to the server, send the packet, and then read in the return packet.
+    struct sockaddr_in serv_addr; // Server address data structure.
+    struct hostent *server;      // Server data structure.
+    sockfd = socket( AF_INET, SOCK_DGRAM, IPPROTO_UDP ); // Create a UDP socket.
+    if ( sockfd < 0 ) {
+        printf( "ERROR opening socket\n" );
+        return -1;
+    }
+    server = gethostbyname( host_name ); // Convert URL to IP.
+    printf("Connecting to %s\n", host_name);
+    if ( server != NULL ) {
+        // Zero out the server address structure.
+        bzero( ( char* ) &serv_addr, sizeof( serv_addr ) );
+        serv_addr.sin_family = AF_INET;
+    // Copy the server's IP address to the server address structure.
+
+        bcopy( ( char* )server->h_addr, ( char* ) &serv_addr.sin_addr.s_addr, server->h_length );
+        // Convert the port number integer to network big-endian style and save it to the server address structure.
+        serv_addr.sin_port = htons( portno );
+        // Call up the server using its IP address and port number.
+        if ( connect( sockfd, ( struct sockaddr * ) &serv_addr, sizeof( serv_addr) ) < 0 ) {
+            printf( "ERROR connecting to socket\n" );
+            return -1;
+        }
+        // Send it the NTP packet it wants. If n == -1, it failed.
+        n = write( sockfd, ( char* ) &packet, sizeof( ntp_packet ) );
+        if ( n < 0 ) {
+            printf( "ERROR writing to socket\n" );
+            return -1;
+        }
+        // Wait and receive the packet back from the server. If n == -1, it failed.
+        n = read( sockfd, ( char* ) &packet, sizeof( ntp_packet ) );
+        if ( n < 0 ) {
+            printf( "ERROR reading from socket" );
+            return -1;
+        }
+    }
+    else {
+        printf( "ERROR, no such host\n" );
+        return -1;
+    }
+
+    // These two fields contain the time-stamp seconds as the packet left the NTP server.
+    // The number of seconds correspond to the seconds passed since 1900.
+    // ntohl() converts the bit/byte order from the network's to host's "endianness".
+
+    packet.txTm_s = ntohl( packet.txTm_s ); // Time-stamp seconds.
+    packet.txTm_f = ntohl( packet.txTm_f ); // Time-stamp fraction of a second.
+    printf("Time in seconds %d\n", packet.txTm_s);
+    // Extract the 32 bits that represent the time-stamp seconds (since NTP epoch) from when the packet left the server.
+    // Subtract 70 years worth of seconds from the seconds since 1900.
+    // This leaves the seconds since the UNIX epoch of 1970.
+    // (1900)------------------(1970)**************************************(Time Packet Left the Server)
+
+    time_t txTm = ( time_t ) ( packet.txTm_s - NTP_TIMESTAMP_DELTA );
+
+    // Print the time we got from the server, accounting for local timezone and conversion from UTC time.
+
+    //printf( "Time: %s", ctime( ( const time_t* ) &txTm ) );
+
+    return txTm;
+}
