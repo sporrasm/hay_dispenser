444886d14e8736c9871ed0e60c6d7d73a9518846
sporrasm <porrasmaa@gmail.com>
2021-09-13 21:01:22 +0300

    Time string to local time conversion works

--- main/horse_feeder_main.c
+++ main/horse_feeder_main.c
@@ -19,6 +19,7 @@
 #define LCD_ROWS 2
 
 void LCD_updater(void* param);
+void timeFromString();
 
 static const char* TAG="MAIN";
 
@@ -78,15 +79,11 @@ void app_main(void)
     wifi_conn_stat=conn_wifi();
     if (wifi_conn_stat != 0) {
         printf("Connection failed!\n");
-        //printf("Trying again!\n");
-        //vTaskDelay(5000 / portTICK_PERIOD_MS);
+        printf("Please restart uC!\n");
     }   
     else {
         printf("Connected to WiFi\n");
         printf("Getting current time\n");
-        //sntp_setoperatingmode(SNTP_OPMODE_POLL);
-        //sntp_setservername(0, "pool.ntp.org");
-        //sntp_init();
         time_t currtime = get_time();
         ESP_LOGI(TAG, "Setting system time to %s", ctime(&currtime));
         struct timeval tv;
@@ -96,8 +93,12 @@ void app_main(void)
         LCD_clearScreen();
         LCD_writeStr("PonyFeeder 3000");
         LCD_setCursor(0, 1);
+        const char times[] = "Mon Sep 13 10:00:00 2021";
         xTaskCreate(&LCD_updater, "LCD_updater", 2048, NULL, 5, NULL);
-        //while (1) {
+        while (1) {
+          timeFromString();
+          vTaskDelay(2000 / portTICK_RATE_MS);
+        }
         //  vTaskDelay(10000 / portTICK_RATE_MS);
         //  //struct timeval curr;
         //  time_t now = time(NULL);
@@ -111,6 +112,50 @@ void app_main(void)
     }
 }
 
+void timeFromString() {
+    char* times[] = {"10:00", "12:00"};
+    struct tm currtime,desttime;
+    memset(&desttime, 0, sizeof(struct tm));
+    memset(&currtime, 0, sizeof(struct tm));
+    time_t now = time(NULL);
+    if (localtime_r(&now, &currtime) == NULL) {
+      printf("localtime_r failed\n");
+      return;
+    }
+    // Process strings 
+    //char* datebuf=malloc(sizeof(char)*16);
+    char datebuf[80];
+    for (int i=0; i<sizeof(times) / sizeof(times[0]); i++) {
+      //memset(datebuf, 0, sizeof(*datebuf));
+      sprintf(datebuf, "%04d %02d %02d %s", currtime.tm_year+1900, currtime.tm_mon+1, currtime.tm_mday, times[i] );
+      printf("Time set: %s\n", datebuf);
+      if (strptime(datebuf, "%Y %m %d %H:%M", &desttime) == NULL)
+        printf("Strptime failed!\n");
+      time_t ptr = mktime(&desttime);
+      printf("Time: %s\n", ctime(&ptr));
+    }
+    //printf("Time set: %s\n", times);
+    //if (strptime(times, "%a %b %d %H:%M:%S %Y", &time) == NULL)
+    //  printf("Strptime failed!\n");
+    //printf("tm_sec %d\n", time.tm_sec);
+    //printf("tm_min %d\n", time.tm_min);
+    //printf("tm_hour %d\n", time.tm_hour);
+    //printf("tm_mday %d\n", time.tm_mday);
+    //printf("tm_year %d\n", time.tm_year);
+  //char *times[] = {"10:00", "12:00", "13:00", NULL};
+//  }
+}
+
+//void activateFeeder(void* param) {
+//  time_t now = time(NULL);
+//  double seconds = difftime(now, mktime()
+//
+//  //while (1) {
+//  //xEventGroupWaitBits(eg,   
+//
+//  //}
+//}
+
 void LCD_updater(void* param)
 {
     char txtBuf[16];
@@ -118,7 +163,7 @@ void LCD_updater(void* param)
           time_t now = time(NULL);
           struct tm timeinfo;
           localtime_r(&now, &timeinfo);
-          printf("Time with localtime() %02d:%02d:%02d\n",timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
+          //printf("Time with localtime() %02d:%02d:%02d\n",timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
           sprintf(txtBuf, "TIME: %02d:%02d", timeinfo.tm_hour, timeinfo.tm_min);
           LCD_writeStr(txtBuf);
           LCD_setCursor(0, 1);
